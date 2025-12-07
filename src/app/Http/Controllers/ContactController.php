1c5f766a35586bd87c7a6577fcbf8f8f019a4a46
linne <linne7351@gmail.com>
2025-12-07 17:37:19 +0900

    12-07

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function confirm(Request $request)
+    {
+        dd($request -> all());
+    }
+}
